13ab13b586d3cb22e9c0c6a4c6ef9de9eebab1f3
SumetPH <notsumet1@gmail.com>
2026-09-18 10:10:35 +0700

    feat: add root endpoint to return welcome message

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -2,6 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  @Get()
+  getRoot(): { message: string } {
+    return { message: 'Welcome to the API!' };
+  }
+
   @Get('health')
   getHealth(): { status: 'ok' } {
     return { status: 'ok' };
